ae3d841e2d8cf44e077adfef2c2ead72b4a77135
tcabrams <tcabrams@cps.edu>
2020-05-07 18:35:07 -0500

    First Example
    Test example

---
+++ Abrams_FirstApp/Abrams_FirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Abrams_FirstApp
+//
+//  Created by Abrams, Teriah C on 2/10/20.
+//  Copyright © 2020 Abrams, Teriah C. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
